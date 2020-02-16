6149cbfcd02587731352ea124f7fcfc4b8a907c6
daniedev <a.danielpf21@gmail.com>
2020-02-16 17:42:51 +0530

    Anagrams

---
+++ src/main/java/basicproblems/inputoutput/Anagrams.java
@@ -0,0 +1,72 @@
+/**Given two strings, a and b , that may or may not be of the same length, determine the minimum number of character deletions required to make a and b anagrams.
+   Any characters can be deleted from either of the strings.
+
+ Input :
+     test cases,t
+     two strings a and b, for each test case
+     Output:
+     Desired O/p
+     Constraints :
+     string lengths<=10000
+     Note :
+     Anagram of a word is formed by rearranging the letters of the word.
+     For e.g. -> For the word RAM - MAR,ARM,AMR,RMA etc. are few anagrams.
+     SAMPLE INPUT
+     1
+     cde
+     abc
+
+ SAMPLE OUTPUT
+ 4
+
+ */
+
+package basicproblems.inputoutput;
+import java.util.Scanner;
+
+public class Anagrams {
+
+    private static class WordsContainer {
+        String word1 = "", word2 = "";
+    }
+
+    public int findSimilarNumberOfLetters(String firstWord, String secondWord) {
+
+        int count = 0;
+
+        char[] word1 = firstWord.toCharArray();
+        char[] word2 = secondWord.toCharArray();
+
+        for (int i = 0; i < word1.length; i++) {
+            for (int j = 0; j < word2.length; j++) {
+                if (word1[i] == word2[j]) {
+                    count++;
+                    word2[j] = 0;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int makeAnagram(String word1, String word2) {
+        int numberOfSimilarLetters = findSimilarNumberOfLetters(word1, word2);
+        return word1.length() + word2.length() - 2 * numberOfSimilarLetters;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Anagrams anagrams = new Anagrams();
+        int numberOfTestCases = input.nextInt();
+        WordsContainer[] wordsContainer = new WordsContainer[numberOfTestCases];
+        for (int i = 0; i < numberOfTestCases; i++) {
+            wordsContainer[i] = new WordsContainer();
+            wordsContainer[i].word1 = input.next();
+            wordsContainer[i].word2 = input.next();
+        }
+
+        for (WordsContainer container : wordsContainer) {
+            System.out.println(anagrams.makeAnagram(container.word1, container.word2));
+        }
+    }
+}
---
+++ src/test/java/basicproblems/inputoutput/AnagramsTest.java
@@ -0,0 +1,43 @@
+package basicproblems.inputoutput;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AnagramsTest {
+
+    Anagrams subject;
+    String word1,word2;
+
+    @Before
+    public void setSubject(){
+        subject = new Anagrams();
+    }
+
+    @Test
+    public void findSimilarNumberOfLetters_onGivingTwoEntries_returnsNumberOfSimilarLetters(){
+        word1 = "dormitory";
+        word2 = "dirtyroom";
+        Assert.assertEquals(9,subject.findSimilarNumberOfLetters(word1,word2));
+
+        word1 = "ttss";
+        word2 = "sss";
+        Assert.assertEquals(2,subject.findSimilarNumberOfLetters(word1,word2));
+
+        word1 = "abc";
+        word2 = "cde";
+        Assert.assertEquals(1,subject.findSimilarNumberOfLetters(word1,word2));
+    }
+
+    @Test
+    public void makeAnagram_onGivingTwoWords_returnsNumberOfLettersToBeDeletedToMakeThemAnagrams(){
+        word1 = "dormitory";
+        word2 = "dirtyroom";
+        Assert.assertEquals(0,subject.makeAnagram(word1,word2));
+
+        word1 = "abc";
+        word2 = "cde";
+        Assert.assertEquals(4,subject.makeAnagram(word1,word2));
+    }
+
+}
