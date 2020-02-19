52c5b524e474df45c5d43cb0169f7a5e279cbc8b
daniedev <a.danielpf21@gmail.com>
2020-02-19 20:07:51 +0530

    Its Magic Approach - 2

--- src/main/java/basicproblems/inputoutput/ItsMagic.java
+++ src/main/java/basicproblems/inputoutput/ItsMagic.java
@@ -1,8 +1,43 @@
+/**
+Problem URL :
+        https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/its-magic/
+
+Problem Description :
+        ussutu is a world-renowned magician.
+        And recently, he was blessed with the power to remove EXACTLY ONE element from an array.
+
+        Given, an array A (index starting from 0) with N elements.
+        Now, Sussutu CAN remove only that element which makes the sum of ALL the remaining elements exactly divisible by 7.
+
+        Throughout his life, Sussutu was so busy with magic that he could never get along with maths.
+        Your task is to help Sussutu find the first array index of the smallest element he CAN remove.
+
+        Input:
+        The first line contains a single integer N.
+        Next line contains N space separated integers Ak , 0 < k < N.
+
+        Output:
+        Print a single line containing one integer, the first array index of the smallest element he CAN remove, and -1 if there is no such element that he can remove!
+
+        Constraints:
+
+        1 < N < 105
+
+        0 < Ak < 109
+
+        SAMPLE INPUT
+        5
+        14 7 8 2 4
+        SAMPLE OUTPUT
+        1
+*/
+
 package basicproblems.inputoutput;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class ItsMagic {
 
@@ -22,6 +57,26 @@ public class ItsMagic {
         return resultIndex;
     }
 
+    public int returnIndexV2(int[] inputNumbersArray) {
+        int result = -1;
+        ArrayList<InputNumber> inputNumbersList = new ArrayList<>();
+
+        for (int itr1 = 0; itr1 < inputNumbersArray.length; itr1++) {
+            inputNumbersList.add(new InputNumber(inputNumbersArray[itr1], itr1));
+        }
+
+        inputNumbersList.sort(InputNumber.sortByValue);
+        getTotal(inputNumbersArray);
+
+        for (InputNumber number : inputNumbersList) {
+            if ((total - number.getValue()) % 7 == 0) {
+                result = number.getIndex();
+                break;
+            }
+        }
+        return result;
+    }
+
     private void getTotal(int[] inputNumbers) {
         for (int number : inputNumbers) {
             total = total + number;
@@ -39,6 +94,33 @@ public class ItsMagic {
             numbers1[itr] = Integer.parseInt(arrayOfNumbers[itr]);
 
         ItsMagic itsMagic = new ItsMagic();
-        System.out.println(itsMagic.returnIndex(numbers1));
+        System.out.println(itsMagic.returnIndexV2(numbers1));
+    }
+
+    private static class InputNumber {
+
+        int value;
+        int index;
+
+        int getValue() {
+            return value;
+        }
+
+        int getIndex() {
+            return index;
+        }
+
+        InputNumber(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+
+        static Comparator<InputNumber> sortByValue = (number, t1) -> {
+            int number1 = number.getValue();
+            int number2 = t1.getValue();
+
+            return number1 - number2;
+        };
+
     }
 }
--- src/test/java/basicproblems/inputoutput/ItsMagicTest.java
+++ src/test/java/basicproblems/inputoutput/ItsMagicTest.java
@@ -38,4 +38,28 @@ public class ItsMagicTest {
         assertEquals(9999994,result);
     }
 
+    @Test
+    public void returnIndexV2_shouldReturnCorrectIndex() {
+        int[] numbers = {14,7,8,2,4};
+        int result = subject.returnIndexV2(numbers);
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void returnIndexV2_ifExpectedNumberIsPresentBeforeOtherNumbersSatisfyingGivenCondition_shouldReturnCorrectIndex(){
+        int[] numbers = {2,7,8,14,4};
+        int result = subject.returnIndexV2(numbers);
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void returnIndexV2_ifInputArraySizeIsGreaterAndNumbersPresentIsAlsoGreater_shouldReturnCorrectIndex(){
+        int[] numbers = new int[10000000];
+        for(int i = 0;i<numbers.length;i++){
+            numbers[i] = numbers.length - i;
+        }
+        int result = subject.returnIndexV2(numbers);
+        assertEquals(9999994,result);
+    }
+
 }
