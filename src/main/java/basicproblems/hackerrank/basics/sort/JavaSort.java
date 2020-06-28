f497af653c80a3831eb55ef8dfbaccedf1602ded
daniedev <a.danielpf21@gmail.com>
2020-06-28 19:32:42 +0530

    Java Sort

---
+++ src/main/java/basicproblems/hackerrank/basics/sort/JavaSort.java
@@ -0,0 +1,68 @@
+package basicproblems.hackerrank.basics.sort;
+
+import java.util.*;
+
+class Student implements Comparable<Student> {
+    private int id;
+    private String fname;
+    private double cgpa;
+
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if (o.cgpa == cgpa) {
+            if (o.fname.equals(fname))
+                return id - o.id;
+            else
+                return fname.compareTo(o.fname);
+
+        }
+        return o.cgpa - cgpa > 0 ? 1 : -1;
+    }
+}
+
+
+//Complete the code
+public class JavaSort {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+        while (testCases > 0) {
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+        Collections.sort(studentList);
+        for (Student st : studentList) {
+            System.out.println(st.getFname());
+        }
+    }
+}
+
+
+
